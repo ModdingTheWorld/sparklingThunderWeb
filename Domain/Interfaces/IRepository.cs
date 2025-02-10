e29ababa5413b173ad8df7534bf647740ab833c1
werwolfy <raphael.nigl@outlook.de>
2025-02-10 14:45:59 +0100

    [TASK] Added Domain layer

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    TEntity? Create(TEntity entity);
+    List<TEntity>? CreateRange(List<TEntity> entities);
+    void Update(TEntity entity);
+    void UpdateRange(List<TEntity> entities);
+    TEntity? Read(int id);
+    List<TEntity>? Read(Expression<Func<TEntity, bool>> predicate);
+    List<TEntity>? Read(int start, int count);
+    List<TEntity>? ReadAll();
+    void Delete(int id);
+    void Delete(TEntity entity);
+}
