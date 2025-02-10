86ab45e38b84727f211deb403a73013243ac2920
werwolfy <raphael.nigl@outlook.de>
2025-02-10 15:27:49 +0100

    [TASK] Started ARepository.cs

---
+++ Domain/Repositories/Implementations/ARepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Domain.Interfaces;
+
+namespace Domain.Repositories.Implementations;
+
+public abstract class ARepository<TEntity>: IRepository<TEntity> where TEntity : class{
+    public TEntity? Create(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<TEntity>? CreateRange(List<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateRange(List<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<TEntity>? Read(Expression<Func<TEntity, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<TEntity>? Read(int start, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<TEntity>? ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
